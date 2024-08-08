b4e1acc8e932aaef49984c2d28ce3768e7118502
Torben Bang Nielsen <tbn@bani.dk>
2024-08-08 05:12:22 +0000

    Bucket creation

--- main.tf
+++ main.tf
@@ -12,6 +12,24 @@ terraform {
   }
 }
 
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "carried-marlin-surfacing"
+}
+
+resource "aws_s3_bucket_ownership_controls" "test-bucket" {
+  bucket = aws_s3_bucket.test-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test-bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.test-bucket]
+
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = "private"
+}
+
 data "aws_s3_bucket" "selected" {
   bucket = "deniable-darkish-squad"
 }
